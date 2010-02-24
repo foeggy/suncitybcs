9c9174fcb8bed8aa4f5e4a9266c9910e83225a76
Geoffrey <foeggy@gschiff.(none)>
2010-02-24 00:05:03 -0600

    addition of the styling and stores to front page

--- app/views/stores/javascript/_index_doc_load.js
+++ app/views/stores/javascript/_index_doc_load.js
@@ -1,7 +1,37 @@
-var latlng = new google.maps.LatLng(-34.397, 150.644);
-var myOptions = {
-	zoom: 8,
+var latlng;
+var myOptions;
+var map;
+var geocoder = new google.maps.Geocoder();
+
+geocoder.geocode( { 'address': "913-D Harvey Road, College Station, TX 77840"}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map, 
+          position: results[0].geometry.location
+      });
+    }})
+
+latlng = new google.maps.LatLng(-34.397, 150.644);
+myOptions = {
+	zoom: 16,
 	center: latlng,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 };
-var map = new google.maps.Map(document.getElementById("map_canvas_1"), myOptions);
+map = new google.maps.Map(document.getElementById("map_canvas_0"), myOptions);
+
+latlng = new google.maps.LatLng(-34.397, 150.644);
+myOptions = {
+	zoom: 16,
+	center: latlng,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+map = new google.maps.Map(document.getElementById("map_canvas_1"), myOptions);
+
+latlng = new google.maps.LatLng(-34.397, 150.644);
+myOptions = {
+	zoom: 16,
+	center: latlng,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+map = new google.maps.Map(document.getElementById("map_canvas_2"), myOptions);
